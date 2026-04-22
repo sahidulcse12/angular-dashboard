031da42513d08abdeb1211486b4c91e2eab25b79
Md Sahidul Islam Muhit <76570068+sahidulcse12@users.noreply.github.com>
2026-04-22 18:08:05 +0600

    refactor user details component

--- src/app/features/user/user-details/user-details.ts
+++ src/app/features/user/user-details/user-details.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-details.html',
   styleUrl: './user-details.css',
 })
-export class UserDetails {}
+export class UserDetails implements OnInit {
+  userId : string | null = null;
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(param=>{
+      this.userId = param.get('id');
+      // console.log('User Id : ',this.userId);
+    })
+  }
+}
